5ac0273a9ae9d0165b5ecb9ec05286accd0211b4
BlueBerry2606 <su8716@srmist.edu.in>
2026-03-10 23:57:18 +0530

    printing oops in ascii using inner class as Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,53 +1,78 @@
 public class OOPSBannerApp {
-    
-    public static String[] getOPattern(){
-        return new String[]{"   ***   ",
-                            " **   ** ",
-                            "**     **",
-                            "**     **",
-                            "**     **",
-                            " **   ** ",
-                            "   ***   "
 
-        };
-    }
-    public static String[] getPPattern(){
-        return new String[] {" ******  ",
-                             " *    *  ",
-                             " ******  ",
-                             " *       ",
-                             " *       ",
-                             " *       ",
-                             " *       "
+    static class CharacterPatternMap {
 
-        };
-    }
-    public static String[] getSPattern(){
-        return new String[] {"   ***   ",
-                             " **      ",
-                             "  **     ",
-                             "    ***  ",
-                             "       **",
-                             " **    **",
-                             "   ***   "
+        private final char character;
+        private final String[] pattern;
 
-        };
+        public CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
     }
-  
-    public static void main(String[] args){
-        String[] oPattern = getOPattern();
-        String[] pPattern = getPPattern();
-        String[] sPattern = getSPattern();
-        
-        for (int i = 0; i < oPattern.length;i++){
-            System.out.println(
-                oPattern[i]+" "+
-                oPattern[i]+" "+
-                pPattern[i]+" "+
-                sPattern[i]
-            );
+
+    public static CharacterPatternMap getOPattern() {
+        return new CharacterPatternMap('O', new String[] {
+            " ******* ",
+            "*       *",
+            "*       *",
+            "*       *",
+            "*       *",
+            "*       *",
+            " ******* "
+        });
+    }
+
+    public static CharacterPatternMap getPPattern() {
+        return new CharacterPatternMap('P', new String[] {
+            "******** ",
+            "*       *",
+            "*       *",
+            "******** ",
+            "*        ",
+            "*        ",
+            "*        "
+        });
+    }
+
+    public static CharacterPatternMap getSPattern() {
+        return new CharacterPatternMap('S', new String[] {
+            " ******* ",
+            "*        ",
+            "*        ",
+            " ******* ",
+            "        *",
+            "        *",
+            " ******* "
+        });
+    }
+
+    public static void main(String[] args) {
+
+        CharacterPatternMap[] chars = new CharacterPatternMap[] {
+            getOPattern(),
+            getOPattern(),
+            getPPattern(),
+            getSPattern()
+        };
+
+        for (int row = 0; row < 7; row++) {
+            StringBuilder line = new StringBuilder();
+            for (int col = 0; col < chars.length; col++) {
+                line.append(chars[col].getPattern()[row]);
+                if (col < chars.length - 1) {
+                    line.append("  ");
+                }
+            }
+            System.out.println(line.toString());
         }
     }
 }
-
-
